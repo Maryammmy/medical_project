2ea852efe9347f83efffaa73f4fb0f22a5f3c02e
Maryammmy <malaksameh18112004@gmail.com>
2024-05-13 04:07:53 +0300

    o

--- src/Components/AllocateDriver/AllocateDriver.jsx
+++ src/Components/AllocateDriver/AllocateDriver.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function AllocateDriver() {
   return (
-    <div className='vh-100'>AllocateDriver</div>
+    <div>AllocateDriver</div>
    
   )
 }
--- src/Components/Userlayout/Userlayout.jsx
+++ src/Components/Userlayout/Userlayout.jsx
@@ -8,7 +8,7 @@ export default function Userlayout() {
   return (
    <div className='d-flex '>
   <div className='bg-color'><Sidebar/></div>
-  <div style={{width: isOpen? "84%" :'99%'}}><Outlet/></div>
+  <div style={{width: isOpen? "84%" :'99%'}} className='height'><Outlet/></div>
      </div>
   )
 }
